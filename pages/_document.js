7c63fd081e654a836f243efcedeb705ae0fd8b4f
Suejin Kim <kim.suejin.20@gmail.com>
2022-09-15 12:38:58 -0400

    add google analytics script

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,24 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=G-F4YK0DEJL8`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-F4YK0DEJL8', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
