a4e05bd55dec857acfc6f46c185a5869e5ffd7c0
Matt Watson <mwatson@renci.org>
2023-06-02 16:49:46 -0400

    build out example nih card usage on updates template

--- pages/latest-updates/[title].js
+++ pages/latest-updates/[title].js
@@ -4,6 +4,14 @@ import Layout from "@/components/layout"
 import Seo from "@/components/elements/seo"
 import { useRouter } from "next/router"
 import Markdown from "react-markdown"
+import { NihCard } from "../../components/nih-card"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faRightToBracket as LearnMoreIcon,
+  faKey as AccessIcon
+} from "@fortawesome/free-solid-svg-icons"
+import { ListGroup } from 'react-bootstrap'
+import CustomLink from '../../components/elements/custom-link'
 
 export default function LatestUpdatePage({ sections, metadata, global }) {
   // Get the path to determine what content to show on the dynamic page
@@ -39,6 +47,59 @@ export default function LatestUpdatePage({ sections, metadata, global }) {
       {/* Display content sections */}
 
       <section className="container mt-6">
+        <div style={{ display: 'flex', gap: '2rem' }}>
+          <NihCard title="This is Test Card #1" seamlessHeader>
+            Commodo in velit adipisicing cupidatat Lorem mollit excepteur.
+            Qui id sunt consectetur exercitation excepteur velit dolore ad
+            velit cupidatat eu eiusmod. Incididunt nisi proident veniam nisi.
+            Quis eiusmod eu nulla magna sunt dolor ex nostrud nostrud nisi
+            do est ullamco id.
+          </NihCard>
+          <NihCard
+            title="This is Test Card #2"
+            titleIcon={<FontAwesomeIcon icon={AccessIcon} />}
+            seamlessHeader
+          >
+            Commodo in velit adipisicing cupidatat Lorem mollit excepteur.
+            Qui id sunt consectetur exercitation excepteur velit dolore ad
+            velit cupidatat eu eiusmod. Incididunt nisi proident veniam nisi.
+            Quis eiusmod eu nulla magna sunt dolor ex nostrud nostrud nisi
+            do est ullamco id.
+          </NihCard>
+          <NihCard
+            title="This is Test Card #3"
+            titleIcon={ <FontAwesomeIcon icon={LearnMoreIcon} width="6x" /> }
+            titleUrl="https://example.com/"
+          >
+            Commodo in velit adipisicing cupidatat Lorem mollit excepteur.
+            Qui id sunt consectetur exercitation excepteur velit dolore ad
+            velit cupidatat eu eiusmod. Incididunt nisi proident veniam nisi.
+            Quis eiusmod eu nulla magna sunt dolor ex nostrud nostrud nisi
+            do est ullamco id.
+          </NihCard>
+        </div>
+        
+        <br />
+        <br />
+
+        <NihCard title="Triple-wide Card" style={{ width: '100%'}}>
+          <ListGroup horizontal>
+            <div style={{ flex: 1, padding: '0 0.5rem', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              <CustomLink link={{ url: "/explore" }}>Explore RADx Data</CustomLink>
+            </div>
+            <svg width="2" height="60"><line x1="1" y1="0" x2="1" y2="60" stroke="#00496e" strokeWidth="2"/></svg>
+            <div style={{ flex: 1, padding: '0 0.5rem', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              <CustomLink link={{ url: "/launch" }}>Launch the RADx Data Hub</CustomLink>
+            </div>
+            <svg width="2" height="60"><line x1="1" y1="0" x2="1" y2="60" stroke="#00496e" strokeWidth="2"/></svg>
+            <div style={{ flex: 1, padding: '0 0.5rem', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              <CustomLink link={{ url: "/docs" }}>Read the Documentation</CustomLink>
+            </div>
+          </ListGroup>
+        </NihCard>
+
+        <hr />
+
         <h1 className="mt-[48px] mb-[8px] text-3xl text-aquaBlue font-bold">
           {latestUpdate.title}
         </h1>
