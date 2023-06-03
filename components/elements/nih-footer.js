1f7300a83208d084aac0329a095d016e25df46ba
suejinkim20 <kim.suejin.20@gmail.com>
2023-06-03 01:44:46 -0400

    fix id property on link prop

--- components/elements/nih-footer.js
+++ components/elements/nih-footer.js
@@ -53,24 +53,24 @@ const NIHFooter = () => {
             </div>
           </Col>
           <Col xs={12} sm={12} md={6} lg={3}>
-            <CustomLink id={1} link={{url: '/contact'}}>
+            <CustomLink link={{url: '/contact', id:'1'}}>
               <div>
                 Contact
               </div>
             </CustomLink>
-            <CustomLink id={2} link={{url: '/faqs'}}>
+            <CustomLink link={{url: '/faqs', id:'1'}}>
               <div>
                 FAQ
               </div>
             </CustomLink>
           </Col>
           <Col xs={12} sm={12} md={6} lg={3}>
-            <CustomLink id={3} link={{url: 'https://www.nih.gov/research-training/medical-research-initiatives/radx'}}>
+            <CustomLink link={{url: 'https://www.nih.gov/research-training/medical-research-initiatives/radx', id:'1'}}>
               <div>
                 RADx Program Website
               </div>
             </CustomLink>
-            <CustomLink id={4} link={{url: 'https://www.hhs.gov/vulnerability-disclosure-policy/index.html'}}>
+            <CustomLink link={{url: 'https://www.hhs.gov/vulnerability-disclosure-policy/index.html', id:'1'}}>
               <div>
                 HHS Vulnerability Disclosure
               </div>
