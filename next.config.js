c149c3c21fdb21360737f4546052eb5d928b16c8
Sergio Rocha <rochascript@gmail.com>
2023-08-09 13:27:29 -0600

    Update next.config.js
    Added a redirect from staging to the new BAH deployment

--- next.config.js
+++ next.config.js
@@ -5,5 +5,14 @@ module.exports = {
   },
   images: {
     domains: ['radx-images.s3.amazonaws.com'],
-  }
+  },
+  async redirects() {
+    return [
+      {
+        source: '/:slug*',
+        destination: 'https://test.radx-hub.nih.gov/info',
+        permanent: true,
+      },
+    ]
+  },
 }
